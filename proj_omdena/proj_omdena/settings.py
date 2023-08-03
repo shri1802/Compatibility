bcaed610fd9ac36628ce7b30778f2bd0af2f7666
Srinidhi Iyer <75072489+shri1802@users.noreply.github.com>
2023-08-03 08:58:12 +0530

    Update settings.py

--- proj_omdena/proj_omdena/settings.py
+++ proj_omdena/proj_omdena/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$jsvgz9k%^vft%_!=)c%_u!o$oph1@0i3ck)8f%4s4%c=_mp6e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
